d115a63c167efe829b5b50a3e1f90d330556883f
Your Name <liyonghzeng@2548722906@qq.com>
2019-05-19 13:48:35 +0000

    -

--- app/Http/Controllers/AlipayController.php
+++ app/Http/Controllers/AlipayController.php
@@ -145,9 +145,8 @@ class AlipayController extends Controller
         $p = json_encode($_POST);
         $log_str = "\n>>>>>> " .date('Y-m-d H:i:s') . ' '.$p . " \n";
         file_put_contents('logs/alipay_notify.log',$log_str,FILE_APPEND);
-        $c = json_decode($p);
-        $cc=$p['name'];
-        echo $cc;
+
+        print_r($p);
 //        echo 'success';
         //TODO 验签 更新订单状态
 
